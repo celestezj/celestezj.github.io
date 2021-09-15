9404c2b402d4d033d37b564755d05e3495882e3e
muggledy <zgjsycfndy2015@163.com>
2021-09-15 19:36:21 +0800

    daiyang's resume

--- js/index.js
+++
@@ -1,8 +0,0 @@
-/* Shapes */
-var svgContainer = document.getElementById('svgContainer');
-var animItem = bodymovin.loadAnimation({
-  wrapper: svgContainer,
-  animType: 'svg',
-  loop: true,
-  path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
-});
